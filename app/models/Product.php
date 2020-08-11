808d7c08c55b5ee480217bf1bf54adc9cf746d74
alex8716 <aleksandarmarkoviccrni@gmail.com>
2020-08-11 08:42:17 +0200

    Json

--- app/models/Product.php
+++ app/models/Product.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Core\DB;
+use JsonSerializable;
 
-class Product extends Model
+class Product extends Model implements JsonSerializable
 {
 
     protected $db, $table = "products", $modelName = 'Product';
@@ -13,5 +14,10 @@ class Product extends Model
     public function __construct()
     {
         $this->db = new DB;
+    }
+
+    public function jsonSerialize()
+    {
+        return ['id' => $this->id, 'name' => $this->name, 'description' => $this->description, 'category' => $this->category];
     }
 }
